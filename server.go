11c53efbaa570599fed4c9cfa27b655fb017aa62
Maia <zgovorovski@gmail.com>
2020-10-03 12:09:26 +0100

    Added ability to ping CF DNS

--- server.go
+++ server.go
@@ -9,15 +9,20 @@ import (
 
 func webServer() string {
 	e := echo.New()
-	// e.GET("/", func(c echo.Context) error {
-	// 	return c.String(http.StatusOK, "Hello, World!")
-	// })
-	e.HideBanner = true
+	e.GET("/ping", func(c echo.Context) error {
+		_, err := http.Get("https://cloudflare-dns.com/dns-query")
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		return c.String(http.StatusOK, "I can access the internet!")
+	})
+
 	assetHandler := http.FileServer(rice.MustFindBox("./app").HTTPBox())
 	e.GET("/*", echo.WrapHandler(assetHandler))
 
 	// Start server
 	port := "57943"
+	e.HideBanner = true
 	go e.Start(":" + port)
 	return "http://localhost:" + port
 }
